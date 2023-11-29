326b96a831d0ed63712c6aeb963f2f4fd3b35d24
trantam280304 <trantam27280304@gmail.com>
2023-11-29 10:07:11 +0700

    lession

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LessionController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,5 @@ Route::get('/', function () {
 use App\Http\Controllers\TenController;
 
 Route::resource('categories', CategoryController::class);
+
+Route::resource('lession', LessionController::class);
