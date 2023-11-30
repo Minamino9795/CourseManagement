fef7b5a6c9628df56faf125dd7ba49c6329fa149
Minamino9795 <minamino9795@gmail.com>
2023-11-30 08:35:00 +0700

    update

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -5,11 +5,16 @@
                 <a href="index.html" class="menu-link"><span class="menu-icon fas fa-home"></span> <span
                         class="menu-text">Trang chủ</span></a>
             </li>
-            <li class="menu-item has-child">
-                <a href="#" class="menu-link">
-					<span class="menu-icon far fa-file"></span> 
-					<span class="menu-text">Khóa học</span> 
-				</a>
+            <li class="menu-header">Danh Mục </li>
+            <li class="menu-item has-child" style="">
+                <a href="#" class="menu-link"><span class="menu-icon"><i class="fas fa-users"></i></span>
+                    <span class="menu-text">Danh mục Khóa học</span></a> <!-- child menu -->
+                <ul class="menu">
+                    <li class="menu-subhead">Danh mục</li>
+                    <li class="menu-item">
+                        <a href="" class="menu-link" tabindex="-1">Danh Sách</a>
+                    </li>
+                </ul>
             </li>
         </ul>
     </nav>
