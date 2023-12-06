386f2572511fb1f93e5675e8784c7703859e6ed4
phunguyen392 <nguyenphu.126@gmail.com>
2023-12-06 16:59:43 +0700

    ud

--- app/Http/Controllers/ChapterController.php
+++ app/Http/Controllers/ChapterController.php
@@ -12,84 +12,62 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 class ChapterController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-	public function index(Request $request)
-{
-    $limit = $request->limit ? $request->limit : 12;
-    $query = Chapter::select('*');
-
-    if (isset($request->s)) {
-        $query->whereHas('course', function ($query) use ($request) {
-            $query->where('name', 'like', "%{$request->s}%");
-        });
+    public function index(Request $request)
+    {
+        $limit = $request->limit ? $request->limit : 12;
+        $query = Chapter::select('*');
+        if (isset($request->s)) {
+            $query->whereHas('course', function ($query) use ($request) {
+                $query->where('name', 'like', "%{$request->s}%");
+            });
+        }
+        if ($request->name) {
+            $query->where('name', $request->name);
+        }
+        if ($request->course_id) {
+            $query->where('course_id', $request->course_id);
+        }
+        $query->orderBy('id', 'DESC');
+        $items = $query->with('course')->paginate($limit);
+        $params = [
+            'items' => $items,
+        ];
+        return view('admin.chapters.index', $params)->with('success', __('sys.search_item_success'));
     }
-
-    if ($request->name) {
-        $query->where('name', $request->name);
-    }
-
-    if ($request->course_id) {
-        $query->where('course_id', $request->course_id);
-    }
-
-    $query->orderBy('id', 'DESC');
-    $items = $query->with('course')->paginate($limit);
-
-    if ($items->isEmpty()) {
-        return redirect()->back()->with('error', __('sys.search_item_error'));
-    } else {
-        return view('admin.chapters.index', ['items' => $items])->with('success', __('sys.search_item_success'));
-    }
-}
-
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-		$courses = Course::all(); 
-		$params = 
-		[
-			'courses' => $courses,
-		];
-        return view ('admin.chapters.create',$params);
-		
+        $courses = Course::all();
+        $params = [
+            'courses' => $courses,
+        ];
+        return view('admin.chapters.create', $params);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-       try
-       {
-        // dd($request->all());
-        $item = new Chapter();
-          $request->validate([
-			'name' => ['required', 'unique:chapters,name,NULL,id,course_id,' . $request->course_id],
-			// Các quy tắc validate khác
-		]);
-		$item->name = $request->name;
-        $item->course_id = $request->course_id;
-        $item->save();
-		Log::info('Chapter store successfully. ID: ' . $item->id);
+        try {
+            // dd($request->all());
+            $item = new Chapter();
+            $request->validate([
+                'name' => ['required', 'unique:chapters,name,NULL,id,course_id,' . $request->course_id],
+                // Các quy tắc validate khác
+            ]);
+            $item->name = $request->name;
+            $item->course_id = $request->course_id;
+            $item->save();
+            Log::info('Chapter store successfully. ID: ' . $item->id);
 
-        return redirect()->route('chapters.index')->with('success',__('sys.store_item_success'));
-       }
-       catch (QueryException $e)
-       {
-		Log::error($e->getMessage());
-		dd($e->getMessage()); 
-        return redirect()->route('chapters.index')->with('error',__('sys.store_item_error'));
-        
-       }
+            return redirect()
+                ->route('chapters.index')
+                ->with('success', __('sys.store_item_success'));
+        } catch (QueryException $e) {
+            Log::error($e->getMessage());
+            dd($e->getMessage());
+            return redirect()
+                ->route('chapters.index')
+                ->with('error', __('sys.store_item_error'));
+        }
     }
-
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
@@ -100,20 +78,20 @@ class ChapterController extends Controller
      */
     public function edit(string $id)
     {
-		try {
+        try {
             $item = Chapter::findOrFail($id);
-			$courses = Course::all();
+            $courses = Course::all();
             $params = [
                 'item' => $item,
-				'courses' => $courses,
+                'courses' => $courses,
             ];
-            return view("admin.chapters.edit", $params);
-            } 
-        catch (ModelNotFoundException $e) 
-            {
+            return view('admin.chapters.edit', $params);
+        } catch (ModelNotFoundException $e) {
             Log::error($e->getMessage());
-            return redirect()->route('chapters.index')->with('error', __('sys.item_not_found'));
-            }
+            return redirect()
+                ->route('chapters.index')
+                ->with('error', __('sys.item_not_found'));
+        }
     }
 
     /**
@@ -121,23 +99,26 @@ class ChapterController extends Controller
      */
     public function update(Request $request, string $id)
     {
-		try
-        {
+        try {
             $item = Chapter::findOrFail($id);
             $item->name = $request->name;
             $item->course_id = $request->course_id;
-			$item->save();
-            return redirect()->route('chapters.index')->with('success', __('sys.update_item_success'));
-
-		}
-		catch (ModelNotFoundException $e) {
+            $item->save();
+            return redirect()
+                ->route('chapters.index')
+                ->with('success', __('sys.update_item_success'));
+        } catch (ModelNotFoundException $e) {
             Log::error($e->getMessage());
-            return redirect()->route('chapters.index')->with('error', __('sys.item_not_found'));
+            return redirect()
+                ->route('chapters.index')
+                ->with('error', __('sys.item_not_found'));
         } catch (QueryException $e) {
             Log::error($e->getMessage());
-			dd($e->getMessage());
-			
-            return redirect()->route('chapters.index')->with('error', __('sys.update_item_error'));
+            dd($e->getMessage());
+
+            return redirect()
+                ->route('chapters.index')
+                ->with('error', __('sys.update_item_error'));
         }
     }
 
@@ -146,16 +127,22 @@ class ChapterController extends Controller
      */
     public function destroy(string $id)
     {
-		try {
+        try {
             $item = Chapter::findOrFail($id);
             $item->delete();
-            return redirect()->route('chapters.index')->with('success', __('sys.destroy_item_success'));
+            return redirect()
+                ->route('chapters.index')
+                ->with('success', __('sys.destroy_item_success'));
         } catch (ModelNotFoundException $e) {
             Log::error($e->getMessage());
-            return redirect()->route('chapters.index')->with('error', __('sys.item_not_found'));
-        } catch (QueryException  $e) {
+            return redirect()
+                ->route('chapters.index')
+                ->with('error', __('sys.item_not_found'));
+        } catch (QueryException $e) {
             Log::error($e->getMessage());
-            return redirect()->route('chapters.index')->with('error', __('sys.destroy_item_error'));
+            return redirect()
+                ->route('chapters.index')
+                ->with('error', __('sys.destroy_item_error'));
         }
     }
 }
